7eaaab8bc3df4e4faee32deb919cd530bc47cd49
Jonathan Harding <jonathan.harding@derivco.co.uk>
2016-05-25 09:15:34 +0100

    Add draft powershell script

---
+++ PackageBuilder.ps1
@@ -0,0 +1,20 @@
+﻿Param(
+    [string]$projectFilePath,
+    [string]$projectFileName
+)
+
+$projectFileLocation = "$projectFilePath\$projectFileName"
+$etlOutputFolder = "$projectFilePath\ETL"
+
+$projectFile = New-Object XML
+$packageToAdd = New-Object XML
+$projectFile.Load($projectFileLocation)
+
+Get-ChildItem $etlOutputFolder -Recurse -Filter *.dtsx |
+    ForEach-Object {
+        Write-Host $_.FullName
+        $packageToAdd.Load($_.FullName)
+        $projectFile.project.deploymentmodelspecificcontent.manifest.project.packages.AppendChild($_.Name)
+    }
+
+$projectFile.project.deploymentmodelspecificcontent.manifest.project.packages
