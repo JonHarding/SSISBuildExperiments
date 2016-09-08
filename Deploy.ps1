86784db5b94b3182c2e12dae8b3bf5b7cf3f97b7
Jonathan Harding <jonathan.harding@derivco.co.uk>
2016-09-08 10:31:03 +0100

    Add Find-DacFile function

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,6 +1,49 @@
 param([string]$targetConnectionString, [string]$Dacpac, [string]$targetDatabaseName, [string]$Profile)
  
 #$dacfxPath = 'C:\Program Files (x86)\Microsoft SQL Server\120\DAC\bin\Microsoft.SqlServer.Dac.dll'
+<#
+ .SYNOPSIS
+ Finds the DAC File that you specify
+ .DESCRIPTION
+ Looks through the supplied PathList array and searches for the file you specify.  It will return the first one that it finds.
+ .PARAMETER FileName
+ Name of the file you are looking for
+ .PARAMETER PathList
+ Array of Paths to search through.
+ .EXAMPLE
+ Find-DacFile -FileName "Microsoft.SqlServer.TransactSql.ScriptDom.dll" -PathList @("${env:ProgramFiles}\Microsoft SQL Server", "${env:ProgramFiles(x86)}\Microsoft SQL Server")
+#>
+Function Find-DacFile {
+	Param(
+		[Parameter(Mandatory=$true)]
+		[string]$FileName,
+		[Parameter(Mandatory=$true)]
+		[string[]]$PathList
+	)
+
+	$File = $null
+
+	ForEach($Path in $PathList)
+	{
+		Write-Debug ("Searching: {0}" -f $Path)
+
+		If (!($File))
+		{
+			$File = (
+				Get-ChildItem $Path -ErrorAction SilentlyContinue -Filter $FileName -Recurse |
+				Sort-Object FullName -Descending |
+				Select -First 1
+				)
+
+			If ($File)
+			{
+				Write-Debug ("Found: {0}" -f $File.FullName)
+			}
+		}
+	}
+
+	Return $File
+}
  
 $logs = "C:\DacpacReport"
  
